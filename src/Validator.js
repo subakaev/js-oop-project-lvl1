2acdcd1b35a470d5a8aa1981a6ebd6c60fac4865
Sergei Bakaev <bakaev@yandex.ru>
2021-10-17 22:30:26 +0300

    add addValidator base

--- src/Validator.js
+++ src/Validator.js
@@ -3,22 +3,38 @@ import NumberSchema from './schemas/NumberSchema';
 import ObjectSchema from './schemas/ObjectSchema';
 import StringSchema from './schemas/StringSchema';
 
-function Validator() {}
+function Validator() {
+  this.customValidators = {
+    string: {},
+    number: {},
+    array: {},
+    object: {},
+  };
+}
 
 Validator.prototype.string = function getStringSchema() {
-  return new StringSchema();
+  return new StringSchema(this.customValidators.string);
 };
 
 Validator.prototype.number = function getNumberSchema() {
-  return new NumberSchema();
+  return new NumberSchema(this.customValidators.number);
 };
 
 Validator.prototype.array = function getArraySchema() {
-  return new ArraySchema();
+  return new ArraySchema(this.customValidators.array);
 };
 
 Validator.prototype.object = function getObjectSchema() {
-  return new ObjectSchema();
+  return new ObjectSchema(this.customValidators.object);
+};
+
+Validator.prototype.addValidator = function addValidator(
+  schemaName,
+  validatorName,
+  validate,
+) {
+  // TODO check schema before add?
+  this.customValidators[schemaName][validatorName] = validate;
 };
 
 export default Validator;
