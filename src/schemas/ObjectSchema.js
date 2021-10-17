08e4a56ab3a64319fcddb3a5dc57db9f895d180d
Sergei Bakaev <bakaev@yandex.ru>
2021-10-17 22:23:48 +0300

    ObjectSchema base impl

--- src/schemas/ObjectSchema.js
+++ src/schemas/ObjectSchema.js
@@ -1,15 +1,27 @@
-function ObjectSchema() {}
+import _ from 'lodash';
+
+function ObjectSchema() {
+  this.shapeSchema = {};
+}
 
 // TODO remove
 // eslint-disable-next-line no-unused-vars
 ObjectSchema.prototype.shape = function addShapeChecks(shapeSchema) {
+  // TODO check shape schema type?
+  this.shapeSchema = shapeSchema;
   return this;
 };
 
 // TODO remove
 // eslint-disable-next-line no-unused-vars
 ObjectSchema.prototype.isValid = function isValid(value) {
-  return false;
+  if (!_.isObject(value)) {
+    return false; // TODO check array?
+  }
+
+  const keys = Object.keys(this.shapeSchema);
+
+  return keys.every((key) => this.shapeSchema[key].isValid(value[key]));
 };
 
 export default ObjectSchema;
